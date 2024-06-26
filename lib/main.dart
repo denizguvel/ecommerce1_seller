57f771c6638249b2eb06b69047972f0131062e9e
BazingaTesla <deniz.guvel@gmail.com>
2024-06-26 21:03:10 +0300

    Gradle and Compile SDK version problems were solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
-import 'package:flutter/material.dart';
+import 'package:ecommerce1_seller/views/auth_screen/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
-void main() {
+import 'const/const.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key : key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+    return const GetMaterialApp(
+      title: appname,
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/views/auth_screen/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:ecommerce1_seller/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: purpleColor,
+      body: Container(),
+    );
+  }
+}
